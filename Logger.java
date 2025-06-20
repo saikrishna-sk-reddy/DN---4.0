01f323a2e4a08f3b5bd6e3c19413595ed44cdcd3
saikrishna-sk-reddy <2210030311@klh.edu.in>
2025-06-20 16:36:44 +0530

    Add files via upload

--- Logger.java
+++ Logger.java
@@ -1,21 +1,17 @@
-public class Logger {
-    // Step 1: Create a private static instance of the class
-    private static Logger singleInstance;
 
-    // Step 2: Make the constructor private so it cannot be instantiated externally
+public class Logger {
+    private static Logger singleInstance;
     private Logger() {
         System.out.println("Logger instance created.");
     }
-
-    // Step 3: Provide a public static method to get the instance
     public static Logger getInstance() {
         if (singleInstance == null) {
-            singleInstance = new Logger(); // lazy initialization
+            singleInstance = new Logger();
         }
         return singleInstance;
     }
-
-    public void log(String message) {
+    public void log(String message) 
+    {
         System.out.println("LOG: " + message);
     }
 }
